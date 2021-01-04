5ea0062b25148c79366759a5232dd8af3a9fa676
Mitisha Goyal <mishugoyal7696@gmail.com>
2021-01-04 19:46:20 +0530

    Create ShowHideDiv.js

---
+++ ShowHideDiv.js
@@ -0,0 +1,33 @@
+import { Button } from '@material-ui/core';
+import React, { Component } from 'react';
+class HideShow extends Component {
+    state ={
+        toggleState:false
+    }
+
+    handleToggle =()=>{
+       let toggleNewState= this.state.toggleState;
+        this.setState({toggleState:!toggleNewState})
+    }
+   
+    render()
+    
+
+    {
+        var Data =null;
+        if(this.state.toggleState)
+        {
+            Data=(
+                <div>
+                    <h1>Button Toggled</h1>
+                    </div>
+            )
+        }
+        return (
+            <div>
+            < Button onClick={this.handleToggle}> ToggleButton</Button >
+            {Data}
+            </div>
+        )}
+}
+export default HideShow;
